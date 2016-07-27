552e760647e6202908099196ba825eb178ade2a3
Trevor Sargent <tbird1710@gmail.com>
2016-07-26 22:05:20 -0700

    add comments

---
+++ lib/asciiGraphics.c
@@ -0,0 +1,3 @@
+// ALL FUNCTION CALLS TO ncurses SHOULD BE HERE
+// TODO figure out what tools are useful
+// TODO make tools that are useful 
---
+++ mbrq.c
@@ -0,0 +1,7 @@
+// read in .mbrq file -> story content
+// read in dependencies -> locations, environments, objects, etc. 
+// LOOP 
+	// step through story
+	// display options
+	// process input
+	// chance state
