312fb520e29816bac71b8fb05c4b85ffaa87e2e3
Trevor Sargent <tbird1710@gmail.com>
2016-07-28 23:03:36 -0700

    print the contents of a file to the command line

--- mbrq.c
+++ mbrq.c
@@ -5,3 +5,39 @@
 	// display options
 	// process input
 	// chance state
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE* openFile();
+
+int main(){
+
+	FILE *story = openFile();
+
+	do{
+		printf("%c", fgetc(story));
+	}while(!feof(story) && !ferror(story));
+
+	return 0;
+}
+
+FILE* openFile(){
+
+	char* path = (char*) malloc (128 * sizeof(char));
+
+	printf("Enter the path to the .mbrq file you'd like to open: ");
+	scanf("%127s", path);
+
+	FILE *fr = fopen(path, "r");                                
+    if (fr == NULL) {
+        printf("The file didn't open.\n");
+        return 0;
+    }
+
+	free(path);
+    return fr;
+
+}
